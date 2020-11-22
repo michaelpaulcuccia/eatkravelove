16d1d1ef520684bf612523775a1b7b5d25f48254
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-11-22 13:45:13 -0500

    updated admin login emails

--- auth/admin.js
+++ auth/admin.js
@@ -3,7 +3,7 @@ module.exports = function (req, res, next) {
     //deconstruct req
     const { email } = req.body;
 
-    if (email === 'michael@mail.com') {
+    if (email === 'michael@mail.com' || email === 'ekl_admin@mail.com') {
         req.body.admin = 'admin_001';
     }
 
