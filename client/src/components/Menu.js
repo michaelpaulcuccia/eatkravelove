7870317452a2e17ecbd61655011857798f4c8bee
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-12-11 14:25:26 -0500

    quick update to menu

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -72,32 +72,32 @@ const Menu = () => {
             <div className='flex_container'>
 
                 <div className='menu_box'>
-                    <p className='head'><strong><img className='image' src={india} alt="india" /> Indo-Mexican  <img className='image' src={mex} alt="mex" /></strong></p>
+                    <p className='menu_title'><strong><img className='image' src={india} alt="india" /> Indo-Mexican  <img className='image' src={mex} alt="mex" /></strong></p>
                     {indoMexicanDisplay.map((item, i) => <p key={i}>{item} </p>)}
                 </div>
 
                 <div className='menu_box'>
-                    <p className='head'><strong><img className='image' src={india} alt="india" /> Indo-Chinese <img className='image' src={china} alt="china" /> /  <img className='image' src={thai} alt="thai" /> Thai Inspired</strong></p>
+                    <p className='menu_title'><strong><img className='image' src={india} alt="india" /> Indo-Chinese <img className='image' src={china} alt="china" /> /  <img className='image' src={thai} alt="thai" /> Thai Inspired</strong></p>
                     {indoChineseDisplay.map((item, i) => <p key={i}>{item} </p>)}
                 </div>
 
                 <div className='menu_box'>
-                    <p className='head'><strong> <img className='image' src={india} alt="india" /> Indo-European <img className='image' src={euro} alt="euro" /></strong></p>
+                    <p className='menu_title'><strong> <img className='image' src={india} alt="india" /> Indo-European <img className='image' src={euro} alt="euro" /></strong></p>
                     {indoEuropeanDisplay.map((item, i) => <p key={i}>{item} </p>)}
                 </div>
 
                 <div className='menu_box'>
-                    <p className='head'><strong><img className='image' src={mideast} alt="mideast" /> Middle Eastern</strong></p>
+                    <p className='menu_title'><strong><img className='image' src={mideast} alt="mideast" /> Middle Eastern</strong></p>
                     {middleEasternDisplay.map((item, i) => <p key={i}>{item} </p>)}
                 </div>
 
                 <div className='menu_box'>
-                    <p className='head'><strong>Sandwiches/ Soups/ Salads</strong></p>
+                    <p className='menu_title'><strong>Sandwiches/ Soups/ Salads</strong></p>
                     {sssDisplay.map((item, i) => <p key={i}>{item} </p>)}
                 </div>
 
                 <div className='menu_box'>
-                    <p className='head'><strong><img className='image' src={india} alt="india" /> Indian</strong></p>
+                    <p className='menu_title'><strong><img className='image' src={india} alt="india" /> Indian</strong></p>
                     {indianDisplay.map((item, i) => <p key={i}>{item} </p>)}
                 </div>
 
