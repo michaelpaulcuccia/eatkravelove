1141098125c3737b6c12c422d7560ba4ae403170
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-12-14 08:46:09 -0500

    style updates

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,8 +4,24 @@ const About = () => {
     return (
         <div>
 
-            <div className="container">
+            <div className="container"
+                style={{ fontFamily: 'Architects Daughter, cursive', fontSize: '18px', marginTop: '15px' }}
+            >
                 I have made it my mission to help people understand and care about what they put in their bodies while bringing cultures and cuisines together in the process. Although women have come a long way in terms of rights, we have a long way to go. I feel the best way of getting us the equality we deserve in the world is by lifting each other up and supporting one another to follow our passions. We will make greater strides in making a difference in the world with the love and encouragement from other female entrepreneurs.
+            </div>
+
+            <br></br>
+
+            <div className="container"
+                style={{ fontFamily: 'Architects Daughter, cursive', fontSize: '18px' }}
+            >
+                <strong
+                    style={{ fontFamily: 'Architects Daughter, cursive', fontSize: '24px', marginLeft: '45%' }}
+                >Donation
+                </strong>
+
+                <br></br>
+                We will match your donation and deliver fresh vegetables, fruits and dairy to those in need. We have teamed with 'Be The Change' of Kean University to provide fresh groceries to Middlesex, Essex, and Union Counties. Venmo @SANKETA
             </div>
 
 
