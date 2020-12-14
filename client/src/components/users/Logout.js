7b196d8aac242f2d5093f9bf91b655edc15d3556
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-12-14 08:56:13 -0500

    additional styling

--- client/src/components/users/Logout.js
+++ client/src/components/users/Logout.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, Fragment } from 'react';
 import UserContext from '../../context/UserContext';
 import { useHistory } from 'react-router-dom';
 
@@ -25,20 +25,43 @@ const Logout = props => {
     };
 
     return (
-        <div className="container"
-            style={{ margin: 'auto', marginLeft: '40%', marginTop: '5%' }}
-        >
-            <strong>Are you sure you want to leave?</strong>
-            <br></br>
-            <br></br>
-            <form onSubmit={handleLogOut}
-                style={{ margin: 'auto', marginLeft: '8%' }}
+
+        <Fragment>
+            <div className="container"
+                style={{ margin: 'auto', marginLeft: '39%', marginTop: '8%' }}
             >
-                <button type='submit'
-                    style={{ padding: '2px 2px 2px 2px' }}
-                >Logout</button>
-            </form>
-        </div>
+                <strong
+                    style={{ fontFamily: 'Architects Daughter, cursive', fontSize: '20px' }}
+                >Are you sure you want to leave?</strong>
+                <br></br>
+                <br></br>
+                <form onSubmit={handleLogOut}
+                    style={{ margin: 'auto', marginLeft: '11%' }}
+                >
+                    <button type='submit'
+                        style={{ padding: '3px 3px 3px 3px', color: 'white', backgroundColor: 'red' }}
+                    >Logout</button>
+                </form>
+            </div >
+
+            <div className="container"
+                style={{ margin: 'auto', marginLeft: '35%', marginTop: '5%' }}
+            >
+                <i className='fab fa-instagram'></i>
+                <a
+                    style={{
+                        textDecoration: 'none',
+                        fontFamily: 'Architects Daughter, cursive',
+                        fontSize: '24px'
+                    }}
+                    className='footer_anchortag'
+                    href='https://www.instagram.com/eatkravelove/?hl=en'
+                    target="_blank"
+                    rel="noreferrer"> Be Sure to Follow Us on Instagram
+                </a>
+            </div>
+
+        </Fragment>
 
     )
 }
