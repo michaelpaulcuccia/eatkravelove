bfad15f750dca74677436858eae374d5af2c821e
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-12-15 12:48:03 -0500

    preDeploy

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const keys = require('./productionKeys');
+const path = require('path');
 
 const app = express();
 
 //connect database
+const connectDB = async () => {
+    try {
+        await mongoose.connect(keys.mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
+        console.log('MongoDB Connected')
+    } catch (err) {
+        console.error(err.message);
+        //exit process with failure
+        process.exit(1);
+    }
+}
 connectDB();
 
 //Middleware - get data from req.body in routes
@@ -24,6 +41,15 @@ app.use('/api/indoeuropean', require('./routes/api/indoeuropean'));
 app.use('/api/indomexican', require('./routes/api/indomexican'));
 app.use('/api/middleeastern', require('./routes/api/middleeastern'));
 app.use('/api/sandwichessoupssalads', require('./routes/api/sandwichessoupssalads'));
+
+//serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
