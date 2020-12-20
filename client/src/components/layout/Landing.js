93842670a21e441e77253efdc7c220c5c2155d36
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-12-20 12:28:30 -0500

    update before redeploy

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,9 +1,9 @@
 import React, { Fragment } from 'react';
-import picOne from '../../images/diamond/picOne.PNG';
-import imgThree from '../../images/diamond/imgThree.PNG';
-import picThree from '../../images/diamond/picThree.PNG';
-import picTwo from '../../images/diamond/picTwo.PNG';
-import '../../style/DiamondStyle.css'
+import one from '../../images/landing/one.PNG';
+import two from '../../images/landing/two.PNG';
+import three from '../../images/landing/three.PNG';
+import four from '../../images/landing/four.PNG';
+import '../../style/PicBox.css'
 import Footer from './landingComponents/Footer';
 
 const Landing = () => {
@@ -11,11 +11,11 @@ const Landing = () => {
     return (
         <Fragment>
             <Fragment>
-                <div className='diamond_box'>
-                    <img src={imgThree} alt="" />
-                    <img src={picOne} alt="" />
-                    <img src={picThree} alt="" />
-                    <img src={picTwo} alt="" />
+                <div className='pic_holder'>
+                    <img className="pic" src={one} alt="" />
+                    <img className="pic" src={two} alt="" />
+                    <img className="pic" src={three} alt="" />
+                    <img className="pic" src={four} alt="" />
                 </div>
             </Fragment>
             <Fragment>
